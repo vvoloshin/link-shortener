03b10ab505c77d051f206eac9977035edd5e37df
Voloshin Vitaly <vv.voloshin@severmed.com>
2020-03-26 18:09:56 +0300

    clean code

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -9,9 +9,9 @@ import (
 //кодировка, сохранение строки, возврат хеша
 func EncodeUrl(s storage.Storage) http.Handler {
 	handleFunc := func(w http.ResponseWriter, r *http.Request) {
-		if url := r.PostFormValue("url"); url != "" {
-			hashed := crypto.Hash(url)
-			s.Save(hashed, url)
+		if rawUrl := r.PostFormValue("url"); rawUrl != "" {
+			hashed := crypto.Hash(rawUrl)
+			s.Save(hashed, rawUrl)
 			w.WriteHeader(201)
 			w.Write([]byte(hashed))
 		} else {
@@ -25,8 +25,8 @@ func EncodeUrl(s storage.Storage) http.Handler {
 //поиск по хешу, возврат оригинальной строки
 func DecodeUrl(s storage.Storage) http.Handler {
 	handleFunc := func(w http.ResponseWriter, r *http.Request) {
-		if url := r.PostFormValue("url"); url != "" {
-			if val, err := s.Read(url); err == nil {
+		if hashed := r.PostFormValue("url"); hashed != "" {
+			if val, err := s.Read(hashed); err == nil {
 				w.Write([]byte(val))
 			} else {
 				w.WriteHeader(400)
