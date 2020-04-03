9a254f4eef5cb349c106e4ef41aaba9f0b56e244
Voloshin Vitaly <vv.voloshin@severmed.com>
2020-04-03 15:27:34 +0300

    refactor code

--- crypto/crypto.go
+++ crypto/crypto.go
@@ -3,28 +3,18 @@ package crypto
 import (
 	"github.com/btcsuite/btcutil/base58"
 	"github.com/theckman/go-securerandom"
+	"log"
 )
 
 func GenerateBase58Str() string {
-	rndStr := generateRnd58(10)
-	return cutStringToLimit(rndStr, 8)
+	rndStr := generateRnd58(12)
+	return rndStr[len(rndStr)-8:]
 }
 
 func generateRnd58(n int) string {
-	bytes, _ := securerandom.Bytes(n)
-	return base58.Encode(bytes)
-}
-
-func cutStringToLimit(s string, limit int) string {
-	var res []rune
-	count := 0
-	for _, char := range s {
-		if count == limit {
-			break
-		}
-		res = append(res, char)
-		count++
+	bytes, err := securerandom.Bytes(n)
+	if err != nil {
+		log.Fatal(err)
 	}
-	resShort := string(res)
-	return resShort
+	return base58.Encode(bytes)
 }
